c5e78bc5659418dfc4e962cc254ecf23df2b0a97
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-16 18:14:05 +0000

    Code formatting. Team stats to data_processing

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from modules import data_preprocessing
 from modules import data_processing
+from modules.data_processing import compute_team_stats
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+
 
 def main():
     # Determine the output path
@@ -67,57 +69,11 @@ def main():
         st.pyplot(plt)
 
 
-def compute_team_stats(data):
-    team_stats = []
+# Display the team stats
+st.header("Team Statistics")
+team_stats_df = data_processing.compute_team_stats(data)
+st.dataframe(team_stats_df)
 
-    # Loop over each unique team
-    for team in data['poss'].unique():
-        team_data = data[data['poss'] == team]
-
-        # 1. Total and average yards on first down
-        first_down_data = team_data[team_data['down'] == '1st']
-        total_yards_first_down = first_down_data['yards'].sum()
-        avg_yards_first_down = first_down_data['yards'].mean()
-
-        # 2. Total explosive plays (>= 15 yards), divided into pass and run
-        explosive_data = team_data[team_data['yards'] >= 15]
-        total_explosive_plays = explosive_data.shape[0]
-        explosive_pass_plays = explosive_data[explosive_data['playtype'] == 'Pass'].shape[0]
-        explosive_rush_plays = explosive_data[explosive_data['playtype'] == 'Rush'].shape[0]
-
-        # 3. Percentage of plays that are third down
-        third_down_data = team_data[team_data['down'] == '3rd']
-        total_plays = team_data.shape[0]
-        third_down_plays = third_down_data.shape[0]
-        third_down_percentage = (third_down_plays / total_plays) * 100 if total_plays > 0 else 0
-
-        # Add to the list of team stats
-        team_stats.append([
-            team,
-            total_yards_first_down,
-            avg_yards_first_down,
-            total_explosive_plays,
-            explosive_pass_plays,
-            explosive_rush_plays,
-            total_plays,
-            third_down_plays,
-            third_down_percentage,
-        ])
-
-    # Convert to DataFrame and add column names
-    team_stats_df = pd.DataFrame(team_stats, columns=[
-        'Team',
-        'Total Yards 1st Down',
-        'Avg Yards 1st Down',
-        'Total Explosive Plays',
-        'Explosive Pass Plays',
-        'Explosive Rush Plays',
-        'Total Plays',
-        '3rd Down Plays',
-        '3rd Down %'
-    ])
-
-    return team_stats_df
 
 if __name__ == "__main__":
     main()
