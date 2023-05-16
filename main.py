76ad22fcba0cbb505a5404936b7a979350d348fc
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-16 23:08:29 +0000

    Alignment fixes

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import json
 def main():
     st.markdown("<h1 style='text-align: center;'>Football Statistics</h1>", unsafe_allow_html=True)
 
-    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
-    with open('football_v2.json', 'r') as f:
+    with open('football_v1.json', 'r') as f:
         lottie_dict = json.load(f)
-    st_lottie(lottie_dict, width=400)
-    st.markdown("</div>", unsafe_allow_html=True)
+        st.container()
+        st_lottie(lottie_dict, width=400)
+        st.markdown("</div>", unsafe_allow_html=True)
 
     st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
     uploaded_file = st.file_uploader("Choose a file", type="csv")
