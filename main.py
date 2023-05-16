16425626d19670a742e552b81feeff5f625ea181
m-ranney <38699117+m-ranney@users.noreply.github.com>
2023-05-16 21:09:48 +0000

    Remove test data - add upload feature. Add Lottie

--- main.py
+++ main.py
@@ -5,86 +5,93 @@ from modules.data_processing import load_data, filter_data, compute_team_stats, 
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
-
+from streamlit_lottie import st_lottie
 
 def main():
-    # Determine the output path
-    output_path = './data/avemaria.csv'
+    st.title("Football Statistics")
+
+    st.text(" ")
+
+    lottie_url = "https://assets6.lottiefiles.com/packages/lf20_n918Sz.json"
+    st_lottie(lottie_url, width=400)
   
     # File upload
     uploaded_file = st.file_uploader("Choose a file", type="csv")
     if uploaded_file is not None:
-        # Save the uploaded file to a temporary location
+
+        st.empty()  # This line will clear the previous elements (like the lottie animation) from the page
+        
+      # Save the uploaded file to a temporary location
         input_path = './data/' + uploaded_file.name
         with open(input_path, 'wb') as f:
             f.write(uploaded_file.getbuffer())
         # Preprocess the uploaded file
         data_preprocessing.preprocess_data(input_path, output_path)
 
-    # Load the data
-    data = pd.read_csv(output_path)
-  
-    # Display the team stats
-    st.header("Team Statistics")
-    selected_team = st.selectbox("Select a team", data['poss'].unique())
-    team_stats = data_processing.compute_team_stats(data, selected_team)
-    st.table(pd.DataFrame(team_stats, index=[selected_team]).T)
-
-
-    # Get the explosive play yard threshold from the user
-    st.header("Explosive Plays")
-    explosive_play_yards = st.number_input("Enter the minimum number of yards for a play to be considered explosive", min_value=1, value=15)
-
-    # Compute the explosive play stats, transpose the DataFrame, and display it
-    explosive_play_stats_df = data_processing.compute_explosive_play_stats(data, explosive_play_yards).transpose()
-    st.dataframe(explosive_play_stats_df)
-
-    # Add a selector for the team
-    st.header("Breakdown by Down")
-    unique_teams = list(data['poss'].unique())
-    selected_team = st.selectbox("Select a team", unique_teams)
-
-    # Compute and display the down stats for the selected team
-    down_stats_df = data_processing.compute_down_stats(data, selected_team).transpose()
-    st.dataframe(down_stats_df)
-  
-    # Sidebar for filtering
-    st.sidebar.header("Filter options")
-
-    # Collect filter options
-    unique_poss = list(data['poss'].unique())
-    selected_poss = st.sidebar.multiselect('Select teams', unique_poss, unique_poss)
-
-    unique_down = list(data['down'].unique())
-    selected_down = st.sidebar.selectbox('Select down', unique_down)
-
-    unique_playtype = list(data['playtype'].unique())
-    selected_playtype = st.sidebar.multiselect('Select playtype', unique_playtype, unique_playtype)
-
-    min_yards, max_yards = int(data['yards'].min()), int(data['yards'].max())
-    selected_yards = st.sidebar.slider('Select a range of yards', min_yards, max_yards, (min_yards, max_yards))
-
-    # Filter the data based on user selections
-    filtered_data = data[
-        (data['poss'].isin(selected_poss)) &
-        (data['down'] == selected_down) &
-        (data['yards'] >= selected_yards[0]) & (data['yards'] <= selected_yards[1]) &
-        (data['playtype'].isin(selected_playtype))
-    ]
-
-    # Display filtered data
-    st.header("Filtered Data")
-    st.dataframe(filtered_data)
-
-    # Generate the plot
-    if st.sidebar.button("Generate Plot"):
-        st.header("Total Yards Gained by Each Team")
-        plot_data = filtered_data.groupby(['poss', 'playtype'])['yards'].sum().sort_values()
-        plot_data.unstack().plot(kind='bar')
-        plt.xlabel('Team')
-        plt.ylabel('Total Yards')
-        plt.title('Total Yards Gained by Each Team')
-        st.pyplot(plt)
+        # Load the data
+        data = pd.read_csv(input_path)
+      
+        # Display the team stats
+        st.header("Team Statistics")
+        selected_team = st.selectbox("Select a team", data['poss'].unique())
+        team_stats = data_processing.compute_team_stats(data, selected_team)
+        st.table(pd.DataFrame(team_stats, index=[selected_team]).T)
+    
+    
+        # Get the explosive play yard threshold from the user
+        st.header("Explosive Plays")
+        explosive_play_yards = st.number_input("Enter the minimum number of yards for a play to be considered explosive", min_value=1, value=15)
+    
+        # Compute the explosive play stats, transpose the DataFrame, and display it
+        explosive_play_stats_df = data_processing.compute_explosive_play_stats(data, explosive_play_yards).transpose()
+        st.dataframe(explosive_play_stats_df)
+    
+        # Add a selector for the team
+        st.header("Breakdown by Down")
+        unique_teams = list(data['poss'].unique())
+        selected_team = st.selectbox("Select a team", unique_teams)
+    
+        # Compute and display the down stats for the selected team
+        down_stats_df = data_processing.compute_down_stats(data, selected_team).transpose()
+        st.dataframe(down_stats_df)
+      
+        # Sidebar for filtering
+        st.sidebar.header("Filter options")
+    
+        # Collect filter options
+        unique_poss = list(data['poss'].unique())
+        selected_poss = st.sidebar.multiselect('Select teams', unique_poss, unique_poss)
+    
+        unique_down = list(data['down'].unique())
+        selected_down = st.sidebar.selectbox('Select down', unique_down)
+    
+        unique_playtype = list(data['playtype'].unique())
+        selected_playtype = st.sidebar.multiselect('Select playtype', unique_playtype, unique_playtype)
+    
+        min_yards, max_yards = int(data['yards'].min()), int(data['yards'].max())
+        selected_yards = st.sidebar.slider('Select a range of yards', min_yards, max_yards, (min_yards, max_yards))
+    
+        # Filter the data based on user selections
+        filtered_data = data[
+            (data['poss'].isin(selected_poss)) &
+            (data['down'] == selected_down) &
+            (data['yards'] >= selected_yards[0]) & (data['yards'] <= selected_yards[1]) &
+            (data['playtype'].isin(selected_playtype))
+        ]
+    
+        # Display filtered data
+        st.header("Filtered Data")
+        st.dataframe(filtered_data)
+    
+        # Generate the plot
+        if st.sidebar.button("Generate Plot"):
+            st.header("Total Yards Gained by Each Team")
+            plot_data = filtered_data.groupby(['poss', 'playtype'])['yards'].sum().sort_values()
+            plot_data.unstack().plot(kind='bar')
+            plt.xlabel('Team')
+            plt.ylabel('Total Yards')
+            plt.title('Total Yards Gained by Each Team')
+            st.pyplot(plt)
 
 if __name__ == "__main__":
     main()
